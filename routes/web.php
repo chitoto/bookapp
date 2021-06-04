8f244506ccc98fb5ba8344c828a8c12333aae39a
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-06-05 02:15:54 +0900

    add:Validation

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,14 @@ Route::get('/', function () {
 
 Route::post('/book', function(Request $request){
   $validator = Validator::make($request->all(),[
-    'name' => 'required[max:255]',
+    'name' => 'required | max:255',
   ]);
+  if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+  }
+
   $book = new Book;
   $book->title = $request->name;
   $book->save();
